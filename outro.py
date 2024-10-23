1357348e55b034fb674ee2308e1a886a599a10a5
Moost <111387631+Moost999@users.noreply.github.com>
2024-10-23 12:52:17 -0300

    Add files via upload

---
+++ outro.py
@@ -0,0 +1,248 @@
+import pyautogui
+import time
+
+time.sleep(3)
+
+pyautogui.moveTo(428, 221)
+pyautogui.click()
+time.sleep(1)
+pyautogui.typewrite('831')
+
+#sec click
+pyautogui.moveTo(370, 277)
+time.sleep(2)
+pyautogui.click()
+
+
+# 3 click
+
+pyautogui.moveTo(370, 292)
+time.sleep(2)
+pyautogui.click()
+
+#4click
+pyautogui.moveTo(370, 310)
+time.sleep(2)
+pyautogui.click()
+
+#5click
+
+pyautogui.moveTo(385, 510)
+time.sleep(2)
+pyautogui.click()
+
+##6 click
+
+pyautogui.moveTo(383, 525)
+time.sleep(2)
+pyautogui.click()
+
+# 6 click
+
+pyautogui.moveTo(381, 542)
+time.sleep(2)
+pyautogui.click()
+
+# 7 click 
+pyautogui.moveTo(382, 558)
+time.sleep(2)
+pyautogui.click()
+
+#8click
+
+pyautogui.moveTo(380, 568)
+time.sleep(2)
+pyautogui.click()
+
+# 9 click
+
+pyautogui.moveTo(383, 590)
+time.sleep(2)
+pyautogui.click()
+
+#10 click
+
+pyautogui.moveTo(379, 634)
+time.sleep(2)
+pyautogui.click()
+
+#11 click
+
+pyautogui.moveTo(381, 673)
+time.sleep(2)
+pyautogui.click()
+
+#12 cclick
+
+pyautogui.moveTo(823, 224)
+time.sleep(2)
+pyautogui.click()
+
+#13 click
+pyautogui.moveTo(825, 287)
+time.sleep(2)
+pyautogui.click()
+
+#14 click
+
+pyautogui.moveTo(831, 371)
+time.sleep(2)
+pyautogui.click()
+time.sleep(2)
+# 15click text
+pyautogui.moveTo(983, 345)
+pyautogui.click()
+time.sleep(2)
+pyautogui.hotkey('ctrl', 'a')
+pyautogui.press('backspace')
+time.sleep(1)
+pyautogui.typewrite("Adicionada no Campo de VO")
+
+#16click
+
+pyautogui.moveTo(833, 445)
+time.sleep(2)
+pyautogui.click()
+
+#17 click
+
+pyautogui.moveTo(850, 454)
+time.sleep(2)
+pyautogui.click()
+
+#18click    
+pyautogui.moveTo(834, 607)
+time.sleep(2)
+pyautogui.click()
+
+time.sleep(2)
+#descer a pagina
+pyautogui.moveTo(1277, 671)
+pyautogui.click()
+pyautogui.click()
+pyautogui.click()
+pyautogui.click()
+pyautogui.click()
+pyautogui.click()
+pyautogui.click()
+pyautogui.click()
+pyautogui.click()
+
+time.sleep(1)
+
+pyautogui.moveTo(830, 670)
+pyautogui.click()
+
+#time.sleep(5)
+
+time.sleep(1)
+
+pyautogui.moveTo(366, 652)
+time.sleep(1)
+pyautogui.click()
+
+
+pyautogui.moveTo(1269, 109)
+pyautogui.click()
+pyautogui.click()
+pyautogui.click()
+
+
+time.sleep(1)
+pyautogui.moveTo(479, 143)
+time.sleep(1)
+pyautogui.click()
+
+time.sleep(1)
+pyautogui.moveTo(377, 199)
+time.sleep(1)
+pyautogui.click()
+
+
+time.sleep(1)
+pyautogui.moveTo(374, 396)
+pyautogui.click()
+
+time.sleep(1)
+pyautogui.moveTo(824, 157)
+time.sleep(1)
+pyautogui.click()
+
+time.sleep(1)
+pyautogui.moveTo(824, 208)
+pyautogui.click()
+
+
+time.sleep(1)
+pyautogui.moveTo(822, 612)
+time.sleep(1)
+pyautogui.click()
+
+time.sleep(1)
+
+pyautogui.moveTo(946, 610)
+time.sleep(1)
+pyautogui.click()
+time.sleep(1)
+
+pyautogui.moveTo(1074, 612)
+pyautogui.click()
+time.sleep(1)
+pyautogui.moveTo(1266, 112)
+pyautogui.click()
+pyautogui.click()
+pyautogui.click()
+pyautogui.click()
+
+
+time.sleep(1)
+pyautogui.moveTo(559, 123)
+pyautogui.click()
+time.sleep(1)
+pyautogui.moveTo(378, 131)
+time.sleep(1)
+pyautogui.click()
+time.sleep(1)
+pyautogui.moveTo(375, 274)
+time.sleep(1)
+pyautogui.click()
+
+time.sleep(1)
+pyautogui.moveTo(375, 456)
+time.sleep(1)
+pyautogui.click()
+time.sleep(1)
+pyautogui.moveTo(374, 479)
+time.sleep(1)
+pyautogui.click()
+time.sleep(1)
+pyautogui.moveTo(377, 497)
+time.sleep(1)
+pyautogui.click()
+time.sleep(1)
+pyautogui.moveTo(370, 514)
+time.sleep(1)
+pyautogui.click()
+
+pyautogui.moveTo(870, 121)
+time.sleep(1)
+pyautogui.click()
+
+pyautogui.moveTo(869, 251)
+time.sleep(1)
+pyautogui.click()
+
+pyautogui.moveTo(1283, 115)
+pyautogui.click()
+pyautogui.click()
+pyautogui.click()
+pyautogui.click()
+pyautogui.click()
+pyautogui.click()
+pyautogui.click()
+pyautogui.click()
+pyautogui.click()
+pyautogui.click()
+
+pyautogui.moveTo(604, 115)
+pyautogui.click()
